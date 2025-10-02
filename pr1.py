cd6a590ad1ddaa98372e2607e98b94b427c2d1b8
Maria Grimaldi <maria.grimaldi@edunext.co>
2025-10-02 18:18:36 +0200

    feat: initial commit with lab skeleton

---
+++ pr1.py
@@ -0,0 +1,31 @@
+"""Asignatura: SGDI
+
+Práctica 1: Web Scraping
+
+Autores:
+- MARIA JOSÉ GRIMALDI FERNÁNDEZ
+- NATALIA PALOMA LA ROSA MONTERO
+- IREMAR LUHETSY RIVAS ÁLVAREZ
+
+Declaración de integridad
+Declaramos que esta solución es fruto exclusivamente de nuestro trabajo
+personal. No hemos sido ayudados por ninguna otra persona o sistema automático
+ni hemos obtenido la solución de fuentes externas, y tampoco hemos compartido
+nuestra solución con otras personas de manera directa o indirexcta.
+Declaramos además que no hemos realizado de manera deshonesta ninguna otra
+actividad que pueda mejorar nuestros resultados ni perjudicar los resultados
+de los demás.
+"""
+
+DBENGINES = 'https://db-engines.com/en/ranking'
+QUOTES = 'http://quotes.toscrape.com/search.aspx'
+
+
+def bd_incrementos(umbral):
+	""""""
+
+def citas_celebres(autor):
+    """"""
+
+def tags_por_autor():
+    """"""
